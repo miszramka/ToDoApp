2b38a10b416bcd47ec4639c5259171ede2954751
miszramka <michal.szramka90@gmail.com>
2022-05-13 14:48:37 +0200

    finished

--- routes/posts.js
+++ routes/posts.js
@@ -1,29 +1,64 @@
 const express = require("express");
+const ToDoTask = require("../models/ToDoAppSchema");
 const router = express.Router();
-const Reminder = require("../models/ToDoAppSchema");
 
-// router.get("/", (req, res) => {
-//   //req - request
-//   res.send("We are on posts");
-// });
-
-// router.get("/specific", (req, res) => {
-//   //req - request
-//   res.send("We are on specific post");
-// });
-
-router.post("/", async (req, res) => {
-  //co robi async?
-  const post = new Reminder({
-    title: req.body.title,
-    description: req.body.description,
-  });
-
+//GETS ALL TASKS
+router.get("/", async (req, res) => {
   try {
-    const savedPost = await post.save(); //co robi await?
-    res.json(savedPost);
+    const tasks = await ToDoTask.find();
+    res.json(tasks);
   } catch (err) {
     res.json({ message: err });
   }
 });
+
+//SUBMITS A TASK
+router.post("/", async (req, res) => {
+  //co robi async?
+  const todoTask = new ToDoTask({
+    content: req.body.content,
+  });
+
+  try {
+    //co robi try?
+    const savedTask = await todoTask.save(); //co robi await?
+    res.json(savedTask);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//SPECIFIC TASK// jak działa dynamic parameter?
+router.get("/:taskId", async (req, res) => {
+  try {
+    const task = await ToDoTask.findById(req.params.taskId);
+    res.json(task);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//DELETE SPECIFIC TASK
+router.delete("/:taskId", async (req, res) => {
+  try {
+    const removedTask = await ToDoTask.deleteOne({ _id: req.params.taskId });
+    res.json(removedTask);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//UPDATE SPECIFIC TASK
+router.patch("/:taskId", async (req, res) => {
+  try {
+    const editedTask = await ToDoTask.updateOne(
+      { _id: req.params.taskId },
+      { $set: { content: req.body.content } }
+    );
+    res.json(editedTask);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
 module.exports = router;
